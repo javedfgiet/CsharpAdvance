9050ceaca3bf93b1f694132ba0e6ee9d4f1c2b50
Javed Ahmad <javedfgiet@gmail.com>
2026-08-11 22:06:15 +0530

    fixed type

--- Program.cs
+++ Program.cs
@@ -10,22 +10,6 @@ namespace CsharpAdvance
         static void Main(string[] args)
         {
 
-            var employees = new[]
-                        {
-                new { Name = "Alice", Salary = 50000 },
-                new { Name = "Bob", Salary = 70000 },
-                new { Name = "Charlie", Salary = 60000 },
-                new { Name = "David", Salary = 80000 }
-            };
-
-            //find second heighest Salary
-
-            var salary = employees.OrderByDescending(x => x.Salary).Skip(1).First().Salary;
-            Console.WriteLine($"Second heighest salary is : {salary}");
         }
-    }
-}
-
-
     }
 }
